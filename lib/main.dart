017e220d614808c083535afe8be3d4cf7b7e0aa6
Keshav Kumar <keshavsharma5324@gmail.com>
2025-12-23 23:00:00 +0530

    Support different delimiters:
    To change the delimiter, the beginning of the string will contain a separate line that looks like this: "//[delimiter]\n[numbers…]". For example, "//;\n1;2" where the delimiter is ";" should return 3.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 
 void main() {
-  print(StringCalculator().add('1\n2,3'));
+  print(StringCalculator().add('//;\n1;2'));
 }
 
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    var nums = numbers.split(RegExp('[,|\n]'));
+    String delimiter = ',';
+    if (numbers.startsWith('//')) {
+      int delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+
+    var nums = numbers.split(RegExp('[$delimiter\n]'));
     return nums.map((n) => int.parse(n)).reduce((sum, n) => sum + n);
   }
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -19,4 +19,9 @@ void main() {
   test('handles new lines between numbers', () {
     expect(calculator.add('1\n2,3'), equals(6));
   });
+
+  test('supports custom delimiters', () {
+    expect(calculator.add('//;\n1;2'), equals(3));
+    expect(calculator.add('//|\n1|2|3'), equals(6));
+  });
 }
